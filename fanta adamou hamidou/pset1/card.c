0970cdd2b4f2f00458d4d3e60e02db502c01aa0a
adamouhamidoufanta <47817859+adamouhamidoufanta@users.noreply.github.com>
2019-02-26 15:45:37 -0600

    Homework done!

---
+++ fanta adamou hamidou/pset1/card.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<cs50.h>
+
+int main(void)
+{
+   long_long n;
+   n = get_long_long("card number:");
+   int i=0;
+   while(card<=0'\0')
+   {
+     i++;
+   }
+       printf("%lld\n",n);
+}
---
+++ fanta adamou hamidou/pset1/cash.c
@@ -0,0 +1,65 @@
+#include<stdio.h>
+#include<cs50.h>
+int main(void)
+{
+      int montant=get_int ("montant:");
+  int a=500;
+  int b=250;
+  int c=200;
+  int d=100;
+  int e=50;
+  int f=25;
+  int g=10;
+  int h=5;
+   int i=0;
+   do{
+       montant=get_int("type montant:");
+   }
+   while(montant<0);
+   while(montant>=a)
+       {
+         montant=montant-a;
+         i++;
+         }
+          while(montant>b)
+       {
+         montant=montant-b;
+         i++;
+         }
+         while(montant>c)
+         {
+         montant=montant-c;
+        i++;
+         }
+         while(montant>d)
+         {
+         montant=montant-d;
+         i++;
+         }
+         while (montant>e)
+         {
+         montant=montant-e;
+         i++;
+         }
+         while(montant>f)
+         {
+         montant=montant-f;
+         i++;
+         }
+         while(montant>g)
+         {
+         montant=montant-g;
+         i++;
+         }
+         while(montant>h)
+         {
+         montant=montant-h;
+         i++;
+         }
+         {
+         printf(" le nombre de pieces est %i",i);
+         }
+         }
+
+
+
